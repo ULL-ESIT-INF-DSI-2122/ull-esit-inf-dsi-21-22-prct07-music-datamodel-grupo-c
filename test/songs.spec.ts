5e2c214269e527651de9d0ab7c1876d24b5d6ba2
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-03-30 15:29:05 +0100

    feat: initial spec file of song class tests :gear:

--- test/songs.spec.ts
+++ test/songs.spec.ts
@@ -1,4 +1,13 @@
 import 'mocha';
 import {expect} from 'chai';
 import {Song} from '../src/song';
+import {Group} from '../src/group';
+import {Genre} from '../src/genre';
 
+const  
+
+describe('Song Class tests', () => {
+  describe('Song Constructor', () => {
+    it()
+  });
+});
