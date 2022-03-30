61a38310cc3dfbd062f3410ec836edda99262dd3
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-03-30 15:35:03 +0100

    genre class initial test file

---
+++ test/genre.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Genre} from '../src/genre';
+
+describe('Genre Class tests', () => {
+  describe('Genre Constructor tests', () => {
+    
+  });
+});
