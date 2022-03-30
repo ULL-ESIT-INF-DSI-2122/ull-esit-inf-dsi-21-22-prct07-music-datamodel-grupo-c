18f48b366fd99319fcea9492c73c570f94cb0ed3
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-03-30 15:56:55 +0100

    add first test to genre class

--- test/genre.spec.ts
+++ test/genre.spec.ts
@@ -1,9 +1,19 @@
-import 'mocha';
-import {expect} from 'chai';
-import {Genre} from '../src/genre';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import { Genre } from '../src/genre';
+
+const rnb = new Genre('Rhythm and blues', ['Usher', 'TLC', 'Alicia Keys', 'Beyoncé', 'R. Kelly'], ['Waterfalls', 'My Boo']);
 
 describe('Genre Class tests', () => {
   describe('Genre Constructor tests', () => {
+    it('Genre Attributes', () => {
+      expect(rnb.name).to.be.eql('Rythm and blues');
+    });
+  });
+  describe('Genre Class getters', () => {
     
+  });
+  describe('Genre Class setters', () => {
+
   });
 });
