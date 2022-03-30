ecf8be03ea6ee8bb8469074f200b913af71b021d
alu0101105741 <alu0101105741@ull.edu.es>
2022-03-30 19:39:09 +0100

    feat: add setters test to Artist Class :gear:

--- test/artist.spec.ts
+++ test/artist.spec.ts
@@ -4,10 +4,10 @@ import { Artist } from '../src/artist';
 
 const groups: string[] = ['Animals As Leaders'];
 const genres: string[] = ['Progressive Metal', 'Instrumental'];
-const albums: string[] = ['Parrhesia'];
-const songs: string[] = ['Conflict Cartography', 'Monomyth', 'Red Miso', 'Gestaltzerfall', 'Asahi', 'The Problem of Other Minds', 'Thoughts and Prayers', 'Micro-Aggressions', 'Gordian Naught'];
+const albums: string[] = ['Parrhesia', 'The Madness Of Many'];
+const songs: string[] = ['Gestaltzerfall', 'The Brain Dance'];
 
-const tosinAbasi: Artist = new Artist('Tosin Abasi', groups, genres, albums, songs);
+const tosinAbasi: Artist = new Artist('Tosin Abasi', ['animals as leaders'], ['Progressive Metal'], ['Parrhesia'], ['Gestaltzerfall']);
 
 describe('Artist Class tests', () => {
   describe('Artist Class getters', () => {
@@ -15,19 +15,45 @@ describe('Artist Class tests', () => {
       expect(tosinAbasi.getName()).to.be.eql('Tosin Abasi');
     });
     it('Artist Groups getter', () => {
-      expect(tosinAbasi.getGroups()).to.be.eql(['Animals As Leaders']);
+      expect(tosinAbasi.getGroups()).to.be.eql(['animals as leaders']);
     });
     it('Artist Genres getter', () => {
-      expect(tosinAbasi.getGenres()).to.be.eql(['Progressive Metal', 'Instrumental']);
+      expect(tosinAbasi.getGenres()).to.be.eql(['Progressive Metal']);
     });
     it('Artist Albums getter', () => {
       expect(tosinAbasi.getAlbums()).to.be.eql(['Parrhesia']);
     });
     it('Artist Songs getter', () => {
-      expect(tosinAbasi.getSongs()).to.be.eql(['Conflict Cartography', 'Monomyth', 'Red Miso', 'Gestaltzerfall', 'Asahi', 'The Problem of Other Minds', 'Thoughts and Prayers', 'Micro-Aggressions', 'Gordian Naught']);
+      expect(tosinAbasi.getSongs()).to.be.eql(['Gestaltzerfall']);
     });
     it('Artist Listeners getter', () => {
       expect(tosinAbasi.getListeners()).to.be.equal(0);
+    });
+  });
+  describe('Artist Class setters', () => {
+    it('Artist Name Setter', () => {
+      tosinAbasi.setName('Oluwatosin Ayoyinka Olumide Abasi');
+      expect(tosinAbasi.getName()).to.be.equal('Oluwatosin Ayoyinka Olumide Abasi');
+    });
+    it('Artist Groups setter', () => {
+      tosinAbasi.setGroups(groups);
+      expect(tosinAbasi.getGroups()).to.be.eql(['Animals As Leaders']);
+    });
+    it('Artist Genres setter', () => {
+      tosinAbasi.setGenres(genres);
+      expect(tosinAbasi.getGenres()).to.be.eql(['Progressive Metal', 'Instrumental']);
+    });
+    it('Artist Albums setter', () => {
+      tosinAbasi.setAlbums(albums);
+      expect(tosinAbasi.getAlbums()).to.be.eql(['Parrhesia', 'The Madness Of Many']);
+    });
+    it('Artist Songs setter', () => {
+      tosinAbasi.setSongs(songs);
+      expect(tosinAbasi.getSongs()).to.be.eql(['Gestaltzerfall', 'The Brain Dance']);
+    });
+    it('Artist Listeners setter', () => {
+      tosinAbasi.setListeners(379947);
+      expect(tosinAbasi.getListeners()).to.be.equal(379947);
     });
   });
 });
