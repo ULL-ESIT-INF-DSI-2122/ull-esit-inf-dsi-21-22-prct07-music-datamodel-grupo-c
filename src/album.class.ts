6d299ce16672cb029b05568179981339138bfc2d
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-04 19:40:03 +0100

    Class information

---
+++ src/album.class.ts
@@ -0,0 +1,11 @@
+export class album {
+    private readonly _name: string;
+
+    private readonly _artist: string;
+
+    private readonly _year: number;
+
+    private _genres: string[];
+
+    private _songs: string[];
+}
