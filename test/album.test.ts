37447a66ee3fe6af2858c47e54bf4501d59ef355
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-04 20:20:46 +0100

    Tests Album Attributes

--- test/album.test.ts
+++ test/album.test.ts
@@ -1,3 +1,25 @@
 import { expect } from 'chai';
+import exp from 'constants';
 import { describe, it } from 'mocha';
-import { Song } from '../src/song.class';
+import { isDataView } from 'util/types';
+import { Album } from '../src/album.class';
+
+const laPromesa = new Album(
+  'La Promesa',
+  'Justin Quiles',
+  2017,
+  ['Urban', 'Pop', 'Reggaeton'],
+  ['La Amiga', 'Confesion', 'Vacio', 'Instagram'],
+);
+describe('Album class tests', () => {
+  describe('Album Constructor tests', () => {
+    it('Album Attributes', () => {
+      expect(laPromesa.name).to.be.eql('La Promesa');
+      expect(laPromesa.artist).to.be.eql('Justin Quiles');
+      expect(laPromesa.year).to.be.eql(2017);
+      expect(laPromesa.genres).to.be.eql(['Urban', 'Pop', 'Reggaeton']);
+      expect(laPromesa.songs).to.be.eql(['La Amiga', 'Confesion', 'Vacio', 'Instagram']);
+    });
+  });
+  });
+});
