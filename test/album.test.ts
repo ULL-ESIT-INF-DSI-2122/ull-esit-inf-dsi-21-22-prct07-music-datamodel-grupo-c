e2d18192e58373c5890f8a5ce8cb98d4e430370c
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-04 20:57:17 +0100

    Tests Album add methods

--- test/album.test.ts
+++ test/album.test.ts
@@ -12,13 +12,25 @@ const laPromesa = new Album(
   ['La Amiga', 'Confesion', 'Vacio', 'Instagram'],
 );
 describe('Album class tests', () => {
+  describe('Album class add methods', () => {
+    it('addGenre method', () => {
+      laPromesa.addGenre('Alternative');
+      expect(laPromesa.genres.length).to.be.eql(4);
+      expect(laPromesa.genres.at(-1)).to.be.eql('Alternative');
+    });
+    it('addSong method', () => {
+      laPromesa.addSong('Bailando');
+      expect(laPromesa.songs.length).to.be.eql(5);
+      expect(laPromesa.songs.at(-1)).to.be.eql('Bailando');
+    });
+  });
   describe('Album Constructor tests', () => {
     it('Album Attributes', () => {
       expect(laPromesa.name).to.be.eql('La Promesa');
       expect(laPromesa.artist).to.be.eql('Justin Quiles');
       expect(laPromesa.year).to.be.eql(2017);
-      expect(laPromesa.genres).to.be.eql(['Urban', 'Pop', 'Reggaeton']);
-      expect(laPromesa.songs).to.be.eql(['La Amiga', 'Confesion', 'Vacio', 'Instagram']);
+      expect(laPromesa.genres).to.be.eql(['Urban', 'Pop', 'Reggaeton', 'Alternative']);
+      expect(laPromesa.songs).to.be.eql(['La Amiga', 'Confesion', 'Vacio', 'Instagram', 'Bailando']);
     });
   });
   describe('Album class getters', () => {
@@ -32,10 +44,10 @@ describe('Album class tests', () => {
       expect(laPromesa.year).to.be.eql(2017);
     });
     it('Album genres getter', () => {
-      expect(laPromesa.genres).to.be.eql(['Urban', 'Pop', 'Reggaeton']);
+      expect(laPromesa.genres).to.be.eql(['Urban', 'Pop', 'Reggaeton', 'Alternative']);
     });
     it('Album songs getter', () => {
-      expect(laPromesa.songs).to.be.eql(['La Amiga', 'Confesion', 'Vacio', 'Instagram']);
+      expect(laPromesa.songs).to.be.eql(['La Amiga', 'Confesion', 'Vacio', 'Instagram', 'Bailando']);
     });
   });
   describe('Album class setters', () => {
@@ -48,4 +60,5 @@ describe('Album class tests', () => {
       expect(laPromesa.songs).to.be.eql(['Buscandote', 'Envolver', 'Encendia']);
     });
   });
+
 });
