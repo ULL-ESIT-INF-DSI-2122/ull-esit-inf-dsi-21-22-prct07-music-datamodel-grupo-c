382d31d9bf7862aae53c10000bab2fec7f7ceb9c
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-04 19:55:55 +0100

    Adding constructor, setters and getters

--- src/album.class.ts
+++ src/album.class.ts
@@ -1,11 +1,39 @@
 export class album {
-    private readonly _name: string;
+  private readonly _name: string;
 
-    private readonly _artist: string;
+  private readonly _artist: string;
 
-    private readonly _year: number;
+  private readonly _year: number;
 
-    private _genres: string[];
+  private _genres: string[];
 
-    private _songs: string[];
+  private _songs: string[];
+
+  constructor(
+    name: string,
+    artist: string = '',
+    year: number,
+    genres: string[] = [],
+    songs: string[] = [],
+  ) {
+    this._name = name;
+    this._artist = artist;
+    this._year = year;
+    this._genres = genres;
+    this._songs = songs;
+  }
+
+  get name(): string { return this._name; }
+
+  get artist(): string { return this._artist; }
+
+  get year(): number { return this._year; }
+
+  get genres(): string[] { return this._genres; }
+
+  set genres(genres: string[]) { this._genres = genres; }
+
+  get songs(): string[] { return this._songs; }
+
+  set songs(songs: string[]) { this._songs = songs; }
 }
