85673527a59710cb7ba99149ee6b38191bf79abe
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-05 12:14:35 +0100

    Class Artist Attributes

---
+++ src/artist.class.ts
@@ -0,0 +1,17 @@
+/**
+ * Artist Class.
+ */
+export class Artist {
+  private readonly _name: string;
+
+  private _groups: string[];
+
+  private _genres: string[];
+
+  private _albums: string[];
+
+  private _songs: string[];
+
+  private _listeners: number;
+
+}
