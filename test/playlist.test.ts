3ce809670f7188980f452be6bab3eb24a783ef78
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-05 16:25:51 +0100

    Playlist starting tests

--- test/playlist.test.ts
+++ test/playlist.test.ts
@@ -3,6 +3,11 @@ import { describe, it } from 'mocha';
 import { Playlist } from '../src/playlist.class';
 
 const clasicsSpain = new Playlist(
-  
-)
+  'Clasicos España',
+  ['Princesas', 'Pajaros de Barro', 'Cero', 'Quiero Ser', 'Caminando por la vida', 'Eras Tu', '90 Minutos', 'Son de Amores'],
+  28800,
+  ['Pop', 'Rock', 'Punk', 'Urban', 'Instrumental']
+);
+
+
 
