5a7c3982f934e02a150c96c6fc76b705c5d3e5dc
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-05 12:43:08 +0100

    feat: getter tests for Group class ready :test_tube:

--- test/group.test.ts
+++ test/group.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import { Group } from '../src/group';
+import { Group } from '../src/group.class';
 
 const fitoYFitipaldis = new Group(
   'Fito y Fitipaldis',
@@ -22,8 +22,40 @@ const elCantoDelLoco = new Group(
 
 describe('Group class Tests', () => {
   describe('Group Class Getters', () => {
-    it('', () => {
-
+    it('Name getter', () => {
+      expect(fitoYFitipaldis.name).to.be.eql('Fito y Fitipaldis');
+      expect(elCantoDelLoco.name).to.be.eql('El Canto del Loco');
+    });    
+    
+    it('Artists getter', () => {
+      expect(fitoYFitipaldis.artists).to.be.eql(
+        ['Adolfo Cabrales', 'Javier Alzola', 'Joserra Senperena', 'Carlos Raya', 'Alejandro Climent', 'Daniel Griffin']);
+      expect(elCantoDelLoco.artists).to.be.eql(
+        ['Dani Martín', 'David Otero', 'Chema Ruiz', 'Jandro Velázquez', 'Iván Ganchegui']);
     });
+
+    it('Year getter', () => {
+      expect(fitoYFitipaldis.year).to.be.eql(1998);
+      expect(elCantoDelLoco.year).to.be.eql(1994);
+    });    
+
+    it('Genres getter', () => {
+      expect(fitoYFitipaldis.genres).to.be.eql(
+        ['Rock and roll', 'Blues rock', 'Pop rock', 'Rockabilly']);
+      expect(elCantoDelLoco.genres).to.be.eql(
+        ['Pop rock', 'Pop punk', 'Pop', 'Power pop']);
+    });
+    
+    it('Albums getter', () => {
+      expect(fitoYFitipaldis.albums).to.be.eql(
+        ['A puerta cerrada', 'Los sueños locos', 'Lo más lejos a tu lado', 'Por la boca vive el pez', 'Huyendo contigo de mi']);
+      expect(elCantoDelLoco.albums).to.be.eql(
+        ['El Canto del Loco', 'A contracorriente', 'Estados de ánimo', 'Zapatillas', 'Personas']);
+    });    
+
+    it('monthlyListeners getter', () => {
+      expect(fitoYFitipaldis.monthlyListeners).to.be.eql(2044949);
+      expect(elCantoDelLoco.monthlyListeners).to.be.eql(2622773);
+    });    
   });
 });
