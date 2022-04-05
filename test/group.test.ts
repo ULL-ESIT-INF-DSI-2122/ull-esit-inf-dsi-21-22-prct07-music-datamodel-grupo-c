e03223dfadd72cfe90b50e3473fd2c496ab58765
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-05 12:26:21 +0100

    feat: first group test object

---
+++ test/group.test.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { Group } from '../src/group';
+
+const fitoYFitipaldis = new Group(
+  'Fito y Fitipaldis',
+  ['Adolfo Cabrales', 'Javier Alzola', 'Joserra Senperena', 'Carlos Raya', 'Alejandro Climent', 'Daniel Griffin'],
+  1998,
+  ['Rock and roll', 'Blues rock', 'Pop rock', 'Rockabilly'],
+  ['A puerta cerrada', 'Los sueños locos', 'Lo más lejos a tu lado', 'Por la boca vive el pez', 'Huyendo contigo de mi'],
+  2044949
+)
+
+describe('Group class Tests', () => {
+  describe('Group Class Getters', () => {
+    it('', () => {
+
+    });
+  });
+});
