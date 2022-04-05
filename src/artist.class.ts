7d774d50f17ab9514a88a776c3cdf8556c47483d
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-05 12:23:16 +0100

    Artist getters and setters

--- src/artist.class.ts
+++ src/artist.class.ts
@@ -29,4 +29,29 @@ export class Artist {
     this._songs = songs;
     this._listeners = listeners;
   }
+
+  get name(): string { return this._name; }
+
+  get groups(): string[] { return this._groups; }
+
+  set groups(groups: string[]) { this._groups = groups; }
+
+  get genres(): string[] { return this._genres; }
+
+  set genres(genres: string[]) { this._genres = genres; }
+
+  get albums(): string[] { return this._albums; }
+
+  set albums(albums: string[]) { this._albums = albums; }
+
+  get songs(): string[] { return this._songs; }
+
+  set songs(songs: string[]) { this._songs = songs; }
+
+  get listeners(): number { return this._listeners; }
+
+  set listeners(listeners: number) { this._listeners = listeners; }
+
+  public addListeners(value: number): void { this._listeners += value; }
+
 }
