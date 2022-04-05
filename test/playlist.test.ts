241465237b1e5e4dde0c77e89c7931d01da13e82
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-05 16:58:16 +0100

    Playlist getters tests

--- test/playlist.test.ts
+++ test/playlist.test.ts
@@ -11,7 +11,24 @@ const clasicsSpain = new Playlist(
 
 describe('Playlist Class Tests', () => {
   describe('Playlist getters test', () => {
-    
+    it('Playlist name getter', () => {
+      expect(clasicsSpain.name).to.be.eql('Clasicos España');
+    });
+    it('Playlist songs getter', () => {
+      expect(clasicsSpain.songs).to.be.eql(['Princesas', 'Pajaros de Barro', 'Cero', 'Quiero Ser', 'Caminando por la vida', 'Eras Tu', '90 Minutos', 'Son de Amores']);
+    });
+    it('Playlist duration getter', () => {
+      expect(clasicsSpain.duration).to.be.eql(28800);
+    });
+    it('Playlist hours getter', () => {
+      expect(clasicsSpain.hours).to.be.eql(8);
+    });
+    it('Playlist minutes getter', () => {
+      expect(clasicsSpain.minutes).to.be.eql(480);
+    });
+    it('Playlist genres getter', () => {
+      expect(clasicsSpain.genres).to.be.eql(['Pop', 'Rock', 'Punk', 'Urban', 'Instrumental']);
+    });
   });
 });
 
