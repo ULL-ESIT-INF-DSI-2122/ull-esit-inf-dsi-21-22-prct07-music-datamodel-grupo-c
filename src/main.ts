606dcab42b371f79e1d2dd51db22bd58bfdc4439
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-15 00:35:49 +0100

    feat: new main menu :gear:

--- src/main.ts
+++ src/main.ts
@@ -1,77 +1,75 @@
 import inquirer = require('inquirer');
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'username',
-      message: 'What is your username?',
-      validate(answer) {
-        if (!answer) {
-          return 'Please, fill your name!';
-        }
-        return true;
-      },
-    },
-    {
-      type: 'list',
-      name: 'userChoice',
-      message: 'What do you want to do?',
-      choices: [
-        'List all playlists',
-        'Search a playlist',
-        'Create a new playlist',
-        'Delete a playlist',
-      ],
-    },
-  ])
-  .then((answers) => {
-    if (answers.userChoice === 'List all playlists') {
-      console.log('*All Playlist*');
-    }
+function exit() {
+  console.log('Exit');
+}
 
-    if (answers.userChoice === 'Search a playlist') {
+function run() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'index',
+        message: 'What do you want to do?',
+        choices: [
+          'Artists',
+          'Groups',
+          'Songs',
+          'Albums',
+          'Playlists',
+          new inquirer.Separator(),
+          'Exit',
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.index) {
+        case 'Artists': {
+          console.log('Artists');
+          break;
+        }
+        case 'Groups': {
+          console.log('Groups');
+          break;
+        }
+        case 'Songs': {
+          console.log('Songs');
+          break;
+        }
+        case 'Albums': {
+          console.log('Albums');
+          break;
+        }
+        case 'Playlists': {
+          console.log('Playlists');
+          break;
+        }
+        case 'Exit': {
+          exit();
+          break;
+        }
+
+        default: {
+          break;
+        }
+      }
       inquirer
         .prompt([
           {
-            type: 'list',
-            name: 'sortMethod',
-            message: 'Choose a sorting method',
-            choices: [
-              'Sort by song title',
-              'Sort by artist/group name',
-              'Sort by year',
-              'Sort by song duration',
-              'Sort by genre',
-              'Sort by number of views',
-            ],
+            type: 'confirm',
+            name: 'exitConfirm',
+            message: 'Continue?',
+            default: true,
           },
         ])
-        .then((sortAnswers) => {
-          if (sortAnswers.sortMethod === 'Sort by song title') {
-            inquirer.prompt([
-              {
-                type: 'list',
-                name: 'ascDesc',
-                message: 'Ascending or Descending order?',
-                choices: [
-                  'Ascending',
-                  'Descending',
-                ],
-              },
-            ])
-              .then((ascDescAnswer) => {
-                console.log(ascDescAnswer);
-              });
+        .then((ContinueAnswer) => {
+          if (ContinueAnswer.exitConfirm) {
+            run();
+          } else {
+            exit();
           }
         });
-    }
+    });
+}
 
-    if (answers.userChoice === 'Create a new playlist') {
-      console.log('*Create a new Playlist*');
-    }
-
-    if (answers.userChoice === 'Delete a playlist') {
-      console.log('*Delete a playlist*');
-    }
-  });
+run();
