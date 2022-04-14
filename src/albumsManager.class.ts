a7774d1f47ed56e9fcdf45a0c9f06d4150e14d45
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-14 19:35:12 +0100

    AlbumsManager constructor added

--- src/albumsManager.class.ts
+++ src/albumsManager.class.ts
@@ -1,16 +1,23 @@
-import lowdb from 'lowdb';
-import FileSync from 'lowdb/adapters/FileSync';
-import { Album } from './album.class';
+import lowdb from "lowdb";
+import FileSync from "lowdb/adapters/FileSync";
+import { Album } from "./album.class";
 
 interface AlbumInterface {
-    name: string,
-    artists: string[],
-    year: number,
-    genres: string[],
-    songs: string[];
-    origin: string[],
+  name: string;
+  artists: string[];
+  year: number;
+  genres: string[];
+  songs: string[];
+  origin: string[];
 }
 
 export default class AlbumsManager {
-    
+  private _albums: Album[];
+
+  constructor() {
+    this._albums = [];
+    const albumDb: lowdb.LowdbSync<AlbumInterface> = lowdb(new FileSync("database/database-albums.json"));
+    const serialized = albumDb.get("albums").value();
+    this.deserializeAlbums(serialized);
+  }
 }
