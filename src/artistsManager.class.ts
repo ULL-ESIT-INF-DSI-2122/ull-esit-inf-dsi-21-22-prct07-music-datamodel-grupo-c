033a37c38b89daf61546c2c58f1a25f39e4ab58f
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-14 17:46:43 +0100

    feat: firsts tests pass for ArtistManager class :gear:

---
+++ src/artistsManager.class.ts
@@ -0,0 +1,52 @@
+import lowdb from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync';
+import { Artist } from './artist.class';
+
+interface ArtistInterface {
+  name: string,
+  groups: string[],
+  genres: string[],
+  albums: string[],
+  songs: string[],
+  listeners: number,
+  origin: string,
+}
+
+export default class ArtistsManager {
+  private _artists: Artist[];
+
+  constructor() {
+    this._artists = [];
+    const artistDb: lowdb.LowdbSync <ArtistsManager> = lowdb(new FileSync('database/database-artists.json'));
+    const serialized: any = artistDb.get('artists').value();
+    this.deserializeArtists(serialized);
+  }
+
+  get artists(): Artist[] { return this._artists; }
+
+  set artists(value: Artist[]) { this._artists = value; }
+
+  public preview(): string {
+    let output = 'NAME\t\tGENRES\t\tLISTENERS\n\n';
+    this.artists.forEach((artist) => {
+      output += `${artist.name}\t\t`;
+      output += `${artist.genres.join(', ')}\t\t`;
+      output += `${artist.listeners}\n`;
+    });
+    return output;
+  }
+
+  private deserializeArtists(artists: ArtistInterface[]) {
+    artists.forEach((artist) => {
+      const systemArtists = new Artist(
+        artist.name,
+        artist.groups,
+        artist.genres,
+        artist.albums,
+        artist.songs,
+        artist.listeners,
+      );
+      this._artists.push(systemArtists);
+    });
+  }
+}
--- test/artistsManager.test.ts
+++ test/artistsManager.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
-import ArtistsManager from '../src/ArtistsManager.class';
+import ArtistsManager from '../src/artistsManager.class';
 
 describe('Artists Manager class tests', () => {
   describe('Artists Manager starts with system default artists', () => {
@@ -14,7 +14,7 @@ describe('Artists Manager class tests', () => {
         + 'Bad Bunny\t\tTrap Latino\t\t47800001\n'
         + 'RIMON\t\tR&B\t\t398666\n'
         + 'Cruz Cafuné\t\tHip-Hop\t\t778803\n'
-        + 'Max Doctor\t\tDrum n Bass\t\t30153\n'
+        + 'Wax Doctor\t\tDrum n Bass\t\t30153\n'
         + 'Rolando Laserie\t\tJazz Latino\t\t261460\n'
         + 'Daddy Yankee\t\tReggaeton\t\t43090002\n',
       );
