db07fbbd1081b8ef8cd1849f86c5c99ec808ac26
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-14 12:15:44 +0100

    feat: user choice structure :gear:

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,40 @@ inquirer
     {
       type: 'input',
       name: 'username',
-      message: 'Type your username',
+      message: 'What is your username?',
+      validate(answer) {
+        if (!answer) {
+          return 'Please, fill your name!';
+        }
+        return true;
+      },
+    },
+    {
+      type: 'list',
+      name: 'userChoice',
+      message: 'What do you want to do?',
+      choices: [
+        'List all playlists',
+        'Search a playlist',
+        'Create a new playlist',
+        'Delete a playlist',
+      ],
     },
   ])
   .then((answers) => {
-    console.log(JSON.stringify(answers, null, '  '));
+    if (answers.userChoice === 'List all playlists') {
+      console.log('*All Playlist*');
+    }
+
+    if (answers.userChoice === 'Search a playlist') {
+      console.log('*Search a playlist and sort*');
+    }
+
+    if (answers.userChoice === 'Create a new playlist') {
+      console.log('*Create a new Playlist*');
+    }
+
+    if (answers.userChoice === 'Delete a playlist') {
+      console.log('*Delete a playlist*');
+    }
   });
