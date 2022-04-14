d25e55c83ae6473b97481c569a2af360678c2cd0
jeffperezfrade <alu0101038520@ull.edu.es>
2022-04-14 20:13:42 +0100

    Passing tests :rocket:

--- test/albumsManager.test.ts
+++ test/albumsManager.test.ts
@@ -34,5 +34,55 @@ describe('Album Manager class tests', () => {
     it('Manager object builds with 3 system playlists', () => {
       expect(systemManager.albums.length).to.be.eql(15);
     });
+    it('Manager can output and list albums', () => {
+      expect(systemManager.preview()).to.be.eql(
+        'ALBUMS\n\n'
+        + 'Parrhesia\n'
+        + 'EL ÚLTIMO TOUR DEL MUNDO\n'
+        + 'Digital Tears\n'
+        + 'All Hail\n'
+        + 'VISIÓN TÚNEL\n'
+        + 'Selected Works 94-96\n'
+        + 'Favourite Worst Nightmare\n'
+        + 'Crisis Of Faith\n'
+        + 'Todo el Tiempo...\n'
+        + 'Dissociation\n'
+        + 'Adictiva\n'
+        + 'MÉTELE AL PERREO\n'
+        + 'Sal y Perrea (Remix)\n'
+        + 'EL PONY\n'
+        + 'Don Don (Remix)\n',
+      );
+    });
+  });
+  describe('Manager can operate through albums objects', () => {
+    it('Manager can create new album', () => {
+      systemManager.createAlbum(newAlbum);
+      expect(systemManager.albums.length).to.be.eq(16);
+    });
+    it('Manager cant create existing album', () => {
+      systemManager.createAlbum(newAlbum);
+      expect(systemManager.albums.length).to.be.eq(16);
+    });
+    it('Manager can update existing album', () => {
+      const updatedAlbum: Album = new Album(
+        'What you see is what you get',
+        'Luke Combs',
+        2017,
+        ['Country'],
+        [outThere],
+      );
+      systemManager.updateAlbum(16, updatedAlbum);
+      expect(systemManager.album(16).name).to.be.eq('What you see is what you get');
+    });
+    it('Manager can save album in database', () => {
+      systemManager.saveAlbum(16);
+    });
+    it('Manager cant delete system Album from database', () => {
+      expect(systemManager.deleteAlbum(1)).to.be.false;
+    });
+    it('Manager can delete user Album from database', () => {
+      expect(systemManager.deleteAlbum(16)).to.be.true;
+    });
   });
 });
