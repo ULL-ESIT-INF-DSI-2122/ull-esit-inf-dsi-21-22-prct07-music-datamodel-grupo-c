bffa65640636a14ff4d96de3db0bbe100d9ff8e3
Eric DS <eric.d.s.info@gmail.com>
2022-04-15 11:22:25 +0100

    album: fix album manager song parsing :gear:

--- src/albumsManager.class.ts
+++ src/albumsManager.class.ts
@@ -1,16 +1,8 @@
 import lowdb from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
 import { Album } from './album.class';
+import { AlbumInterface, SongInterface } from './database.interfaces';
 import { Song } from './song.class';
-
-interface AlbumInterface {
-  name: string;
-  artist: string;
-  year: number;
-  genres: string[];
-  songs: Song[];
-  origin: string[];
-}
 
 export default class AlbumsManager {
   private _albums: Album[];
@@ -79,14 +71,31 @@ export default class AlbumsManager {
     return false;
   }
 
+  private getSongs(songs: string[]): Song[] {
+    const songsDb: lowdb.LowdbSync <SongInterface> = lowdb(new FileSync('database/database-songs.json'));
+    const serialized = songsDb.get('songs').value();
+
+    return serialized
+      .filter((song: SongInterface) => songs.includes(song.name))
+      .map((song: SongInterface) => new Song(
+        song.name,
+        song.artist,
+        song.seconds,
+        song.genres,
+        song.single,
+        song.views,
+      ));
+  }
+
   private deserializeAlbums(albums: AlbumInterface[]) {
     albums.forEach((album) => {
+      const songs: Song[] = this.getSongs(album.songs);
       const systemAlbum = new Album(
         album.name,
         album.artist,
         album.year,
         album.genres,
-        album.songs,
+        songs,
       );
       this._albums.push(systemAlbum);
     });
