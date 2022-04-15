63cfc3267e5b8fa6602629f3cc34f204525138b6
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-15 20:20:17 +0100

    feat: first sort method :gear:

--- src/playlist.menu.ts
+++ src/playlist.menu.ts
@@ -58,7 +58,127 @@ export function playlistMenu() {
                 console.clear();
                 playlistMenu();
               } else {
-                console.log(answerSearch);
+                inquirer
+                  .prompt([
+                    {
+                      type: 'list',
+                      name: 'playlistActions',
+                      message: 'Choose an action:',
+                      choices: [
+                        'Sort playlist',
+                        'List playlist',
+                        'Add Song to a playlist',
+                        'Delete Song from a playlist',
+                        new inquirer.Separator(),
+                        'Go Back',
+                      ],
+                    },
+                  ])
+                  .then((playlistActionsAnswers) => {
+                    switch (playlistActionsAnswers.playlistActions) {
+                      case 'Sort playlist': {
+                        inquirer
+                          .prompt([
+                            {
+                              type: 'list',
+                              name: 'sortMenu',
+                              message: 'Choose an sort method:',
+                              choices: [
+                                'By song name',
+                                'By duration',
+                                'By artist',
+                                'By year',
+                                'By genre',
+                                'By number of listeners',
+                                new inquirer.Separator(),
+                                'Go Back',
+                                new inquirer.Separator(),
+                              ],
+                            },
+                          ])
+                          .then((sortAnswers) => {
+                            switch (sortAnswers.sortMenu) {
+                              case 'By song name': {
+                                inquirer
+                                  .prompt([
+                                    {
+                                      type: 'list',
+                                      name: 'nameAscDesc',
+                                      message: 'Asc or Desc?:',
+                                      choices: [
+                                        'Asc',
+                                        'Desc',
+                                        new inquirer.Separator(),
+                                        'Go Back',
+                                      ],
+                                    },
+                                  ])
+                                  .then((nameSortAnswers) => {
+                                    const inx: number = playlistManager.playlists
+                                      .map((playlist) => playlist.name)
+                                      .indexOf(answerSearch.playlistSearch);
+                                    switch (nameSortAnswers.nameAscDesc) {
+                                      case 'Asc': {
+                                        const playlistAux = playlistManager
+                                          .playlist(inx);
+                                        playlistAux.sortBySongName(false);
+                                        console.log(playlistAux.toString());
+                                        break;
+                                      }
+                                      case 'Desc': {
+                                        const playlistAux = playlistManager.playlist(inx);
+                                        playlistAux.sortBySongName(true);
+                                        console.log(playlistAux.toString());
+                                        break;
+                                      }
+                                      default: {
+                                        break;
+                                      }
+                                    }
+                                  });
+                                break;
+                              }
+                              case 'By duration': {
+                                break;
+                              }
+                              // case 'By artist': {
+                              //   break;
+                              // }
+                              // case 'By year': {
+                              //   break;
+                              // }
+                              // case 'By genre': {
+                              //   break;
+                              // }
+                              // case 'By number of listeners': {
+                              //   break;
+                              // }
+                              // case 'Go Back': {
+                              //   break;
+                              // }
+                              default: {
+                                break;
+                              }
+                            }
+                          });
+                        break;
+                      }
+                      // case 'List playlist': {
+                      //   break;
+                      // }
+                      // case 'Add Song to a playlist': {
+                      //   break;
+                      // }
+                      // case 'Delete Song from a playlist': {
+                      //   break;
+                      // }
+                      // case 'Go Back': {
+                      //   break;
+                      // }
+                      default:
+                        break;
+                    }
+                  });
               }
             });
           break;
